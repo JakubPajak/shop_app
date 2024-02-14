b01585f731f2d92f99ba28db8d10af9a2e3f9b46
Jakub Pająk <jakubpajak@MacBook-Pro-Jakub-2.local>
2024-02-14 20:53:47 +0100

    Initial commit. Basics of the application implemented

---
+++ lib/global_variables.dart
@@ -0,0 +1,37 @@
+final products = [
+  {
+    'title': 'OHMA\'s Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/362980.jpg',
+  },
+  {
+    'title': 'OHMA\'s Sandals',
+    'price': 49.90,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/388021.jpg',
+  },
+  {
+    'title': 'OHMA\'s Shoes Bronze',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/obuwie-brazowe-300x225.jpg',
+  },
+  {
+    'title': 'Child\'s Orthotic Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/sabine.jpg',
+  }
+];
+
+final cart = [
+  {
+    'id': '3',
+    'title': 'OHMA\'s Shoes',
+    'price': 44.53,
+    'imageUrl': 'assets/images/obuwie-brazowe-300x225.jpg',
+    'company': 'Bort',
+    'size': 8,
+  }
+];
